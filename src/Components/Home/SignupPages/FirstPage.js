bc98d00b4c8517836565c951154bc9f23b8b49d4
mazhar <mazharulislam611@gmail.com>
2023-09-16 15:35:08 +0600

    signup form

--- src/Components/Home/SignupPages/FirstPage.js
+++ src/Components/Home/SignupPages/FirstPage.js
@@ -1,98 +1,117 @@
 import React from 'react';
-import { Formik } from 'formik';
 import '../../../Css/Signup.css';
+import { useFormik } from 'formik';
+
+const validate = values => {
+    const errors = {};
+
+    if (!values.firstName) {
+        errors.firstName = 'Required';
+    } else if (values.firstName.length > 15) {
+        errors.firstName = 'Must be 15 characters or less';
+    }
+
+    if (!values.lastName) {
+        errors.lastName = 'Required';
+    } else if (values.lastName.length > 20) {
+        errors.lastName = 'Must be 20 characters or less';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+    }
+
+    return errors;
+};
+
 
 const FirstPage = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            firstName: '',
+            lastName: '',
+            email: '',
+        },
+        validate,
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    //check box validation
+
     return (
-        <div className=''>
-            <h2 className='py-3'>Contact Information</h2>
-            <Formik
-                initialValues={{ firstName: '', email: '', password: '' }}
-                validate={values => {
-                    const errors = {};
-                    if (!values.email) {
-                        errors.email = 'Required';
-                    } else if (
-                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                    ) {
-                        errors.email = 'Invalid email address';
-                    }
-                    return errors;
-                }}
-                onSubmit={(values, { setSubmitting }) => {
-                    setTimeout(() => {
-                        alert(JSON.stringify(values, null, 2));
-                        setSubmitting(false);
-                    }, 400);
-                }}
-            >
+        <div className='signup-content'>
+            <h2 className='py-4'>Contact Information</h2>
+            <form className='signup-form' onSubmit={formik.handleSubmit}>
+                <label htmlFor="firstName">First Name <span style={{ color: 'red' }}>*</span></label>
+                <input
+                    className='form-control'
+                    id="firstName"
+                    name="firstName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.firstName}
+                    placeholder='John'
+                />
+                {formik.errors.firstName ? <div>{formik.errors.firstName}</div> : null}
 
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                    /* and other goodies */
-                }) => (
-                    <form className='form-step-1' onSubmit={handleSubmit}>
+                <label htmlFor="lastName">Last Name <span style={{ color: 'red' }}>*</span></label>
+                <input
+                    className='form-control'
+                    id="lastName"
+                    name="lastName"
+                    type="text"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.lastName}
+                    placeholder='Doe'
+                />
+                {formik.errors.lastName ? <div>{formik.errors.lastName}</div> : null}
 
-                        <label for="firstName" class="form-label">First Name <span style={{ color: 'red' }}>*</span></label>
-                        <input type="text" class="form-control" name='firstName' id="firstName" placeholder='First Name' required />
+                <label htmlFor="email">Email Address <span style={{ color: 'red' }}>*</span></label>
+                <input
+                    className='form-control'
+                    id="email"
+                    name="email"
+                    type="email"
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    value={formik.values.email}
+                    placeholder='johndoe12@gmail.com'
+                />
+                {formik.errors.email ? <div>{formik.errors.email}</div> : null}
 
-                        <label for="lastName" className='form-label' >Last Name <span style={{ color: 'red' }}>*</span></label>
-                        <input
-                            type="text"
-                            class="form-control"
-                            name="lastName"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.lastName}
-                            placeholder='Last Name'
-                        />
-                        {errors.email && touched.email && errors.email} <br />
 
-                        <label for="email" className='form-label' >Email Address <span style={{ color: 'red' }}>*</span></label>
-                        <input
-                            class="form-control"
-                            type="email"
-                            name="email"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.email}
-                            placeholder='johndoe12@gmail.com'
-                        />
-                        {errors.email && touched.email && errors.email} <br />
-                        {/* <input
-                            type="password"
-                            name="password"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.password}
-                            placeholder='password'
-                        />
-                        {errors.password && touched.password && errors.password}
-                        <br /> */}
-                        <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required />
-                        <label class="form-check-label" for="invalidCheck2">
-                            By checking this box, you agree to be added to our mailing list. You can opt out at any time.
-                        </label>
-                        <br/>
-                        <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required />
-                        <label class="form-check-label" for="invalidCheck2">
-                            By Checking this box, you agree to our <a href=''>Terms of Service</a> and <a href="">Privacy Policy</a>.
-                        </label>
+                <div className="form-check p-0">
+                    <input
+                        type="checkbox"
+                        name="selectCheckbox"
+                        id="selectCheckbox"
+                    />
+                    <label htmlFor="selectCheckbox" className="form-check-label ms-2">
+                        Are you sure?
+                    </label>
+                </div>
 
-                        <br />
-                        <button type="submit" className='btn btn-primary form-control' disabled={isSubmitting}>
-                            Submit
-                        </button>
-                    </form>
-                )}
+                <div className="form-check p-0">
+                    <input
+                        type="checkbox"
+                        name="conditionCheckbox"
+                        id="conditionCheckbox"
+                    />
+                    <label htmlFor="conditionCheckbox" className="form-check-label ms-2">
+                        Are you sure?
+                    </label>
+                </div>
 
-            </Formik>
+
+                <button className='form-control btn btn-primary' type="submit">Submit</button>
+            </form>
         </div>
     );
 };
