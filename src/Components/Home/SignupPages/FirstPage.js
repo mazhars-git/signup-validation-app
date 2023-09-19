8c04cbaffa3d93cf79eed3aaf4d7596ae4481f96
mazhar <mazharulislam611@gmail.com>
2023-09-19 23:53:57 +0600

    update

--- src/Components/Home/SignupPages/FirstPage.js
+++ src/Components/Home/SignupPages/FirstPage.js
@@ -6,23 +6,22 @@ const validate = values => {
     const errors = {};
 
     if (!values.firstName) {
-        errors.firstName = 'Required';
-    } else if (values.firstName.length > 15) {
-        errors.firstName = 'Must be 15 characters or less';
+        errors.firstName = 'You did not enter your first name';
+    } else if (values.firstName.length > 10) {
+        errors.firstName = 'Must be 10 characters or less';
     }
 
     if (!values.lastName) {
-        errors.lastName = 'Required';
-    } else if (values.lastName.length > 20) {
-        errors.lastName = 'Must be 20 characters or less';
+        errors.lastName = 'You did not enter your last name';
+    } else if (values.lastName.length > 10) {
+        errors.lastName = 'Must be 10 characters or less';
     }
 
     if (!values.email) {
-        errors.email = 'Required';
+        errors.email = 'Enter your email address';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = 'Invalid email address';
     }
-
     return errors;
 };
 
